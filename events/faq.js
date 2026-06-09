3e678983dfd2472777dc7636796856e4fa35a414
deyeka03 <saksakegawe18@gmail.com>
2026-06-10 02:46:54 +0800

    add sistem faq

--- events/faq.js
+++ events/faq.js
@@ -1,23 +1,38 @@
-const { Events } = require("discord.js");
+const autoResponses = [
+    {
+        trigger: ["drama", "derama", "drma"],
+        response: "KALAU ADA DRAMA SILAHKAN MENUJU KE <#1500295305239728188> MAS/MBA"
+    },
+    {
+        trigger: ["laser", "lser"],
+        response: "ADA LASER? WAH SILAHKAN MENUJU KE <#1500295305239728188> KAK"
+    },
+    {
+        trigger: ["keributan", "ribut", "rbut"],
+        response: "ADA DRAMA? WAH SILAHKAN MENUJU KE <#1500295305239728188> KAK"
+    }
+    {
+        trigger: ["cheater", "cheat"],
+        response: "ADA CHEATER SUNG KE <#1500295305239728188> KAK"
+    }
+    {
+        trigger: ["titan"],
+        response: "ADA TITAN? SUNG KE <#1500295305239728188> KAK"
+    }
+];
 
 module.exports = {
-    name: Events.MessageCreate,
+    name: "messageCreate",
 
     async execute(message) {
         if (message.author.bot) return;
 
-        const responses = {
-            "drama": "KALAU ADA DRAMA SILAHKAN MENUJU KE <#1500295305239728188> MAS/MBA",
-            "ada laser": "ADA LASER? WAH SILAHKAN MENUJU KE <#1500295305239728188> KAK",
-            "keributan": "ADA DRAMA? WAH SILAHKAN MENUJU KE <#1500295305239728188> KAK",
-            "cheater": "ADA CHEATER SUNG KE <#1500295305239728188> KAK",
-            "ada titan": "ADA TITAN? SUNG KE <#1500295305239728188> KAK"
-        };
-
         const content = message.content.toLowerCase();
 
-        if (responses[content]) {
-            message.reply(responses[content]);
+        for (const item of autoResponses) {
+            if (item.trigger.includes(content)) {
+                return message.reply(item.response);
+            }
         }
     }
 };
