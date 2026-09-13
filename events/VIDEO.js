735bc026e7dfea2d132346484645c16cc9565289
deyeka03 <saksakegawe18@gmail.com>
2026-09-13 21:21:12 +0700

    video tutor

---
+++ events/VIDEO.js
@@ -0,0 +1,54 @@
+const path = require("path");
+const { AttachmentBuilder } = require("discord.js");
+
+module.exports = (client) => {
+
+    // Role yang diizinkan
+    const ALLOWED_ROLES = [
+        "1452199873989443717",
+        "1452202000593719346"
+    ];
+
+    const TRIGGERS = {
+        tutor: {
+            text: "INI KA YA TUTORNYA",
+            video: "TUTOR.MOV"
+        }
+    };
+
+    client.on("messageCreate", async (message) => {
+
+        if (message.author.bot) return;
+        if (!message.guild) return;
+
+        // Cek role
+        const hasPermission = message.member.roles.cache.some(role =>
+            ALLOWED_ROLES.includes(role.name)
+        );
+
+        if (!hasPermission) return;
+
+        const trigger = TRIGGERS[message.content.toLowerCase()];
+
+        if (!trigger) return;
+
+        const videoPath = path.join(
+            process.cwd(),
+            "VIDEO",
+            trigger.video
+        );
+
+        try {
+            await message.channel.send({
+                content: trigger.text,
+                files: [
+                    new AttachmentBuilder(videoPath)
+                ]
+            });
+        } catch (err) {
+            console.error("[TRIGGER ERROR]", err);
+        }
+
+    });
+
+};
